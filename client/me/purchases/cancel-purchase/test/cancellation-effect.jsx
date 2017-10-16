ccdae703709db05713f677aaf674f9cb4d7fae9b
Paul Sealock <psealock@gmail.com>
2017-10-17 11:03:11 +1300

    explicitly set types

--- client/me/purchases/cancel-purchase/test/cancellation-effect.jsx
+++ client/me/purchases/cancel-purchase/test/cancellation-effect.jsx
@@ -8,7 +8,8 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import { cancellationEffectDetail, cancellationEffectHeadline } from '../cancellation-effect';
-import * as productsValues from 'lib/products-values';
+import { JETPACK_PLANS } from 'lib/plans/constants';
+import { isTheme, isGoogleApps, isJetpackPlan, isDomainMapping } from 'lib/products-values';
 import purchases from 'lib/purchases';
 
 jest.mock( 'lib/products-values', () => ( {} ) );
@@ -58,17 +59,29 @@ describe( 'cancellation-effect', () => {
 			} );
 
 			test( 'should return translation of theme message when product is a theme', () => {
-				productsValues.isTheme = () => true;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isTheme = () => true;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'premium_theme',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					"Your site's appearance will revert to its previously selected theme and you will be refunded %(cost)s."
 				);
 			} );
 
 			test( 'should return translation of g suite message when product is g suite', () => {
-				productsValues.isTheme = () => false;
-				productsValues.isGoogleApps = () => true;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isTheme = () => false;
+				// isGoogleApps = () => true;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'gapps',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					'You will be refunded %(cost)s, but your G Suite account will continue working without interruption. ' +
 						'You will be able to manage your G Suite billing directly through Google.'
@@ -76,10 +89,16 @@ describe( 'cancellation-effect', () => {
 			} );
 
 			test( 'should return translation of jetpack plan message when product is a jetpack plan', () => {
-				productsValues.isTheme = () => false;
-				productsValues.isGoogleApps = () => false;
-				productsValues.isJetpackPlan = () => true;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isTheme = () => false;
+				// isGoogleApps = () => false;
+				// isJetpackPlan = () => true;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: JETPACK_PLANS[ 0 ],
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					'All plan features - spam filtering, backups, and security screening ' +
 						'- will be removed from your site and you will be refunded %(cost)s.'
@@ -87,10 +106,16 @@ describe( 'cancellation-effect', () => {
 			} );
 
 			test( 'should return translation of plan message when product is not a theme, g suite or a jetpack plan', () => {
-				productsValues.isTheme = () => false;
-				productsValues.isGoogleApps = () => false;
-				productsValues.isJetpackPlan = () => false;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isTheme = () => false;
+				// isGoogleApps = () => false;
+				// isJetpackPlan = () => false;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'something-completely-ridiculous',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					'All plan features and custom changes will be removed from your site and you will be refunded %(cost)s.'
 				);
@@ -104,26 +129,44 @@ describe( 'cancellation-effect', () => {
 			} );
 
 			test( 'should return translation of g suite message when product is g suite', () => {
-				productsValues.isGoogleApps = () => true;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isGoogleApps = () => true;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'gapps',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					'Your G Suite account remains active until it expires on %(subscriptionEndDate)s.'
 				);
 			} );
 
 			test( 'should return translation of domain mapping message when product is a domain mapping', () => {
-				productsValues.isGoogleApps = () => false;
-				productsValues.isDomainMapping = () => true;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isGoogleApps = () => false;
+				// isDomainMapping = () => true;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'domain_map',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					'Your domain mapping remains active until it expires on %(subscriptionEndDate)s.'
 				);
 			} );
 
 			test( 'should return translation of plan message when product is not g suite or a domain mapping', () => {
-				productsValues.isGoogleApps = () => false;
-				productsValues.isDomainMapping = () => false;
-				const headline = cancellationEffectDetail( purchase, translate );
+				// isGoogleApps = () => false;
+				// isDomainMapping = () => false;
+				const headline = cancellationEffectDetail(
+					{
+						domain: 'example.com',
+						product_slug: 'something-completely-ridiculous',
+					},
+					translate
+				);
 				expect( headline.text ).to.equal(
 					"Your plan's features remain active until your subscription expires on %(subscriptionEndDate)s."
 				);
